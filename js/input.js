d2043c6cd98787b287f20b4a016cd1b5a3e364e9
Robert Entenman <geek@geekapproach.com>
2014-09-16 11:11:43 -0700

    Stub methods in input.js
    Stub methods created in input.js

--- js/input.js
+++ js/input.js
@@ -1,1 +1,27 @@
-//Junk
+/*	AppInput.js
+	Verion .1 09/16/14
+	Authors: Robert Entenman, Ian Coldwell
+*/
+
+/* Globals */
+var myFirebaseRef =  null;
+var nameField = $('#nameInputField');
+var colorField = $('#colorInputField');
+
+function initFirebase() {
+	//Grabbing a reference to FireBase
+	 myFirebaseRef = new FireBase("https://glaring-fire-5493.firebaseIO.com/AppData");
+}
+
+function writebackData() {
+
+}
+
+/*
+  Dumps a message to console output.
+*/
+function errorLog() {
+	console.log("An error occured.");
+
+}
+
