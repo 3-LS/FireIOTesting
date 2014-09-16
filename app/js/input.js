c299701cde8d121f5af32d59dc63e3da584f2745
Robert Entenman <geek@geekapproach.com>
2014-09-16 11:17:49 -0700

    Push function enabled
    Externalized the div name elements for grabbing data from FB.

--- app/js/input.js
+++ app/js/input.js
@@ -10,13 +10,24 @@ var colorField = $('#colorInputField');
 
 function initFirebase() {
 	//Grabbing a reference to FireBase
-	 myFirebaseRef = new FireBase("https://glaring-fire-5493.firebaseIO.com/AppData");
+	 myFirebaseRef = new FireBase("https://glaring-fire-5493.firebaseIO.com/AppData/");
 }
 
-function writebackData() {
+/*
+ Traps a return/enter keypress from the colorField,
+ signifying the data can be posted.
+ */
+colorField.keypress(function (e) {
 
-}
+	if (e.keyCode == 13) {
+		var name = nameField.val();
+		var color = colorField.val();
 
+		//Push
+		myFirebaseRef.push({name:name, colorPref:color});
+	}
+
+});
 /*
   Dumps a message to console output.
 */
